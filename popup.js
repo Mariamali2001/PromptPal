c30cdcb4b0adfeb2fab9bdf3d9b4bcea48a9cab5
mariam ali <82689042+Mariamali2001@users.noreply.github.com>
2023-08-31 11:26:15 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,68 +1,32 @@
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  const backButton = document.getElementById('back-button');
-  backButton.addEventListener('click', function () {
-    goBack();
-  });
+// document.addEventListener('DOMContentLoaded', function () {
+//   const backButton = document.getElementById('back-button');
+// //   backButton.addEventListener('click', function () {
+// //     goBack();
+// //   });
 
-  function goBack() {
-    window.location.href = 'popup.html';
-  }
+//   function goBack() {
+//     window.location.href = 'popup.html';
+//   }
 
  
-});
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const copyButtons = document.querySelectorAll('.copy-button');
-//   copyButtons.forEach(function (button) {
-//       button.addEventListener('click', function () {
-//           copyCardContent(this);
-//       });
-//   });
-
-//   function copyCardContent(button) {
-//       const card = button.closest('.card');
-//       const paragraphs = card.querySelectorAll('p');
-//       let content = '';
-
-//       paragraphs.forEach(function (paragraph) {
-//           content += paragraph.textContent + '\n\n';
-//       });
-
-//       const tempTextarea = document.createElement('textarea');
-//       tempTextarea.value = content;
-//       document.body.appendChild(tempTextarea);
-//       tempTextarea.select();
-//       document.execCommand('copy');
-//       document.body.removeChild(tempTextarea);
-//   }
-
-//   // Other JavaScript code...
 // });
-// const copyButtons = document.querySelectorAll('.copy-button');
-//   copyButtons.forEach(button => {
-//       button.addEventListener('click', copyContent);
-//   });
 
-//   // Copy content to clipboard when the button is clicked
-//   function copyContent(event) {
-//       const card = event.target.closest('.card');
-//       const content = card.querySelector('.copy-content').textContent;
+document.addEventListener('DOMContentLoaded', function () {
+    const backButton = document.getElementById('back-button');
+    if (backButton) {
+      backButton.addEventListener('click', function () {
+        goBack();
+      });
+    }
+  
+    function goBack() {
+      window.location.href = 'popup.html';
+    }
+  });
 
-//       const tempInput = document.createElement('textarea');
-//       tempInput.value = content;
-//       document.body.appendChild(tempInput);
-//       tempInput.select();
-//       document.execCommand('copy');
-//       document.body.removeChild(tempInput);
 
-//       // Optionally, provide some visual feedback that content was copied
-//       event.target.textContent = 'Copied!';
-//       setTimeout(() => {
-//           event.target.textContent = 'Copy to Clipboard';
-//       }, 2000); // Reset button text after 2 seconds
-//   }
 
     document.addEventListener('DOMContentLoaded', function() {
         const cards = document.querySelectorAll('.card');
